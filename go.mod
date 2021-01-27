1a907b2eb02f389ded4879d1ec7420a8ff93fa21
jerrychang <jerrychang@cyberon.com.tw>
2021-01-27 15:21:40 +0800

    [init]: initial webserver

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module cicd.icu/cyberon/chatroom
 
 go 1.15
+
+require (
+	github.com/gorilla/mux v1.8.0
+	github.com/meatballhat/negroni-logrus v1.1.1
+	github.com/sirupsen/logrus v1.7.0
+	github.com/urfave/negroni v1.0.0
+)
--- main.go
+++ main.go
@@ -1,5 +1,46 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
+	"net/http"
+
+	"github.com/gorilla/mux"
+	negronilogrus "github.com/meatballhat/negroni-logrus"
+	log "github.com/sirupsen/logrus"
+	"github.com/urfave/negroni"
+)
+
+func main() {
+	// set the server parameter
+	host := "127.0.0.1"
+	port := "8080"
+
+	r := mux.NewRouter()
+
+	// set the file server
+	fs := http.FileServer(http.Dir("./public"))
+	// listen js file
+	r.PathPrefix("/js/").Handler(fs)
+	// listen css file
+	r.PathPrefix("/css/").Handler(fs)
+
+	r.HandleFunc("/", indexHandler)
+	/* Create the logger for the web application. */
+	l := log.New()
+
+	n := negroni.New()
+	n.Use(negronilogrus.NewMiddlewareFromLogger(l, "web"))
+	n.UseHandler(r)
+	// Set the parameters for a HTTP server
+	server := http.Server{
+		Addr:    fmt.Sprintf("%s:%s", host, port),
+		Handler: n,
+	}
+
+	log.Fatal(server.ListenAndServe())
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Welcome!")
 }
